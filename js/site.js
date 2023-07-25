1a690bf2e4b219ddc1a545916b40adcd869d8656
Ernest M. Dicks <ernest.dicks@luxinteractive.com>
2023-07-25 18:06:51 -0400

    Finished palindrome app, need to add regex for white spaces and punctuation

--- js/site.js
+++ js/site.js
@@ -1,88 +1,49 @@
 const getValues = () => {
-  let fizzVal = document.getElementById('fizz-value').value;
-  let buzzVal = document.getElementById('buzz-value').value;
-  let stopVal = document.getElementById('stop-value').value;
+  let userString = document.getElementById('user-string').value;
 
-  let convertFizzValToNum = Number(fizzVal);
-  let convertBuzzValToNum = Number(buzzVal);
-  let convertStopValToNum = Number(stopVal);
+  let revString = checkForPalindrome(userString);
 
-  if (convertFizzValToNum != 3 || convertBuzzValToNum != 5 ||
-    isNaN(convertFizzValToNum) || isNaN(convertBuzzValToNum)) {
-
-    Swal.fire({
-      title: 'Oops!',
-      text: 'Fizz can only be 3. Buzz can only be 5. Try your inputs again.',
-      icon: 'error',
-      backdrop: false
-    });
-  }
-  else if (convertStopValToNum > 5000) {
-    Swal.fire({
-      title: 'Oops!',
-      text: 'The stop value cannot be greater than 5000',
-      icon: 'error',
-      backdrop: false
-    });
-  }
-
-  let passFizzBuzzCollection = generateFizzBuzz(convertFizzValToNum, convertBuzzValToNum, convertStopValToNum);
-
-  displayFizzBuzz(passFizzBuzzCollection, convertStopValToNum);
-
+  displayResults(revString, userString);
 
 }
 
-const generateFizzBuzz = (convertFizzValToNum, convertBuzzValToNum, convertStopValToNum) => {
-  let fizzBuzzNumbers = [];
- 
-  for (let i = 1; i <= convertStopValToNum; i++) {
+const checkForPalindrome = (inputString) => {
 
-    if (i % 3 === 0 && i % 5 === 0) {
-      fizzBuzzNumbers.push("FizzBuzz")
-    } else if (i % 5 === 0) {
-      fizzBuzzNumbers.push("Buzz")
-    }
-    else if (i % 3 === 0) {
-      fizzBuzzNumbers.push("Fizz")
-    }
-    else {
-      fizzBuzzNumbers.push(i)
+  var re = /[^A-Za-z0-9]/g;
+  let input = inputString.toLowerCase().replace(re, '');
+  var length = input.length;
+
+  for (var i = 0; i < length / 2; i++) {
+    if (input[i] !== input[length - 1 - i]) {
+      return false;
     }
   }
-
-  return fizzBuzzNumbers;
-
+  return true;
 }
 
-const displayFizzBuzz = (passFizzBuzzCollection) => {
-  let tableHtml = '';
 
-  for (let i = 0; i < passFizzBuzzCollection.length; i++) {
-    let currentNumber = passFizzBuzzCollection[i];
-    let className = '';
-    if (currentNumber === "Fizz") {
-      className = "fizz text-light";
-    }
-    else if (currentNumber === "Buzz") {
-      className = "buzz text-light";
-    }
-    else if (currentNumber === "FizzBuzz") {
-      className = "fizzbuzz text-light";
-    }
-    else {
-      className = ""
-    }
 
-    let tableRowHtml = `<tr><td class="${className}"> ${currentNumber} </td></tr>`
-   
-    tableHtml += tableRowHtml;
+const displayResults = (revString, userString) => {
+  if (revString === true) {
+    document.getElementById('palindrome').textContent = "You entered a Palindrome!";
+    document.getElementById('results').textContent = userString;
+    let inputBox = document.getElementById('user-string');
+    let alertBox = document.getElementById('alert');
+    alertBox.classList.remove('invisible');
+    inputBox.classList.remove('border', 'border-2', 'border-danger-subtle')
+    inputBox.classList.add('border', 'border-2', 'border-success-subtle')
+
+  }
+  else if (revString === false) {
+    document.getElementById('palindrome').textContent = "You did not enter a Palindrome!";
+    document.getElementById('results').textContent = userString;
+    let inputBox = document.getElementById('user-string');
+    let alertBox = document.getElementById('alert');
+    alertBox.classList.remove('invisible');
+    inputBox.classList.remove('border', 'border-2', 'border-success-subtle')
+    inputBox.classList.add('border', 'border-2', 'border-danger-subtle')
+
   }
 
-  document.getElementById("results").innerHTML = tableHtml
-
 }
 
-const clearDisplay = () => {
-  document.getElementById("results").innerHTML = " ";
-}
