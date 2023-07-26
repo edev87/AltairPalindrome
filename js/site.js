8d265afab21b0ee69df23e09c766cf344a39d1bd
Ernest M. Dicks <ernest.dicks@luxinteractive.com>
2023-07-26 12:13:38 -0400

    Modififed JS code for the app to work with a object

--- js/site.js
+++ js/site.js
@@ -8,10 +8,20 @@ const getValues = () => {
 }
 
 const checkForPalindrome = (inputString) => {
+  // let regex = /[^a-z0-9]/gi; replace anything that isnt a-z 0-9, ^ => means not
+  //this removes extra characters
 
   var re = /[^A-Za-z0-9]/g;
   let input = inputString.toLowerCase().replace(re, '');
   var length = input.length;
+
+  // let palindrome = {
+  //   isPalindrome: (revString == userString);
+  //   reversedString: revString;
+  // };
+  // palindrome.isPalindrome = true;
+  // palindrome.reversedString = revString;
+  //let isPalindrome = (revString == userString);
 
   for (var i = 0; i < length / 2; i++) {
     if (input[i] !== input[length - 1 - i]) {
@@ -24,12 +34,13 @@ const checkForPalindrome = (inputString) => {
 
 
 const displayResults = (revString, userString) => {
+  let inputBox = document.getElementById('user-string');
+  let alertBox = document.getElementById('alert');
   if (revString === true) {
     document.getElementById('palindrome').textContent = "You entered a Palindrome!";
     document.getElementById('results').textContent = userString;
-    let inputBox = document.getElementById('user-string');
-    let alertBox = document.getElementById('alert');
-    alertBox.classList.remove('invisible');
+    alertBox.classList.remove('invisible', 'alert-danger');
+    alertBox.classList.add('alert-success');
     inputBox.classList.remove('border', 'border-2', 'border-danger-subtle')
     inputBox.classList.add('border', 'border-2', 'border-success-subtle')
 
@@ -37,9 +48,8 @@ const displayResults = (revString, userString) => {
   else if (revString === false) {
     document.getElementById('palindrome').textContent = "You did not enter a Palindrome!";
     document.getElementById('results').textContent = userString;
-    let inputBox = document.getElementById('user-string');
-    let alertBox = document.getElementById('alert');
-    alertBox.classList.remove('invisible');
+    alertBox.classList.remove('invisible', 'alert-success');
+    alertBox.classList.add('alert-danger');
     inputBox.classList.remove('border', 'border-2', 'border-success-subtle')
     inputBox.classList.add('border', 'border-2', 'border-danger-subtle')
 
